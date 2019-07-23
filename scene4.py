11f4373adcd5cad7e5de97cd1f320fa1b3c214d2
Ricardo Fragoso <ricardfragoso@gmail.com>
2019-07-23 18:33:27 -0300

    Fixing the Hardcoding

--- scene4.py
+++ scene4.py
@@ -9,17 +9,23 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
-gameDisplay = pygame.display.set_mode((800, 600))
+display_width = 800
+display_height = 600
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Alien Attack')
 
 gameExit = False
 
-lead_x = 300
-lead_y = 300
+lead_x = display_width/2
+lead_y = display_height/2
 lead_x_change = 0
 lead_y_change = 0
 
 clock = pygame.time.Clock()
+
+block_size = 10
+FPS = 30
 
 while not gameExit:
     for event in pygame.event.get():
@@ -29,26 +35,28 @@ while not gameExit:
         # It does NOT allow diagonal movements
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                lead_x_change = -10
+                lead_x_change = -block_size
                 lead_y_change = 0
             elif event.key == pygame.K_RIGHT:
-                lead_x_change = 10
+                lead_x_change = block_size
                 lead_y_change = 0
             elif event.key == pygame.K_UP:
-                lead_y_change = -10
+                lead_y_change = -block_size
                 lead_x_change = 0
             elif event.key == pygame.K_DOWN:
-                lead_y_change = 10
+                lead_y_change = block_size
                 lead_x_change = 0
+    if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:
+        gameExit = True
 
     lead_x += lead_x_change
     lead_y += lead_y_change
 
     gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, 10, 10])
+    pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, block_size, block_size])
     pygame.display.update()
 
-    clock.tick(30)
+    clock.tick(FPS)
 
 
 pygame.quit()
